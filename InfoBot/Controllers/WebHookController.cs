a5879db18a61f6563e5caa58ff9f04e814ac8d94
Дмитрий <whitedemon9@gmail.com>
2017-06-10 14:01:14 +0300

    Fix telegram webhook endpoint

---
+++ InfoBot/Controllers/WebHookController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot;
+
+namespace InfoBot.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class WebHookController : Controller
+    {
+        private readonly ITelegramBotClient _botClient;
+
+        public WebHookController(ITelegramBotClient botClient)
+        {
+            _botClient = botClient;
+            _botClient.SetWebhookAsync("http://helloworld-netcore.herokuapp.com/api/webhook").Wait();
+        }
+
+        // POST api/webhook
+        [HttpPost]
+        public async Task<IActionResult> Post(Update update)
+        {
+            var message = update.Message;
+
+            if (message.Type == MessageType.TextMessage)
+            {
+                // Echo each Message
+                await _botClient.SendTextMessageAsync(message.Chat.Id, message.Text);
+            }
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<string> GetAsync()
+        {
+            var result = await _botClient.TestApiAsync();
+            var me = await _botClient.GetMeAsync();
+            return $"Me - {me}. Test result: {result}";
+        }
+    }
+}
--- InfoBot/Startup.cs
+++ InfoBot/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using InfoBot.MessageGenerators.WeatherMessageGenerator;
+using Telegram.Bot;
 
 namespace InfoBot
 {
@@ -26,11 +27,12 @@ namespace InfoBot
         public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public async void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
             services.AddMvc();
             services.AddSingleton<IWeatherMessageGenerator>(new WeatherMessageGenerator(Configuration["ApiKeys:DarkSky"]));
+            services.AddSingleton<ITelegramBotClient>(new TelegramBotClient(Configuration["ApiKeys:Telegram"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
